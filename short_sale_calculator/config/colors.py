4aa3ac9adbeafbd01320ce906fb25ba732ceab1b
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-06-05 16:11:33 +0530

    Adding color Scheme

---
+++ short_sale_calculator/config/colors.py
@@ -0,0 +1,12 @@
+import flet as ft
+
+class ColorScheme:
+    PRIMARY = ft.Colors.BLUE_600
+    SECONDARY = ft.Colors.BLUE_100
+    BACKGROUND = ft.Colors.WHITE
+    SURFACE = ft.Colors.GREY_50
+    TEXT_PRIMARY = ft.Colors.BLACK87
+    TEXT_SECONDARY = ft.Colors.BLACK54
+    SUCCESS = ft.Colors.GREEN_600
+    ERROR = ft.Colors.RED_600
+    BORDER = ft.Colors.GREY_300
