da581ea88d2d3a969d27069c872ac94531883773
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-06-09 23:22:18 +0530

    Reduced Build Size Sola V1.1

--- form-16_generator/build.ps1
+++ form-16_generator/build.ps1
@@ -1,1 +1,9 @@
-flet build windows --project "Sola" --company "Pooja ITR Center" --description "Created by Krish to Automate Form-16 Generation using ITR Format. Created for POOJA ITR CENTER" --product "Sola" --clear-cache --compile-app --compile-packages --cleanup-app --skip-flutter-doctor --module-name .\main.py
+flet build windows `
+--project "Sola" `
+--company "Pooja ITR Center" `
+--description "Created by Krish to Automate Form-16 Generation using ITR Format. Created for POOJA ITR CENTER" `
+--product "Sola" `
+--build-version "1.1" `
+--copyright "Copyright (C) 2025 Pooja ITR Center" `
+--exclude "release, icons, requirements.txt, README.md, certs, test, .venv" `
+--clear-cache --compile-app --compile-packages --cleanup-app --skip-flutter-doctor --module-name .\main.py
