5da40e89538f0d31df0fbc1607fc77c2eab09057
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-06-10 22:11:15 +0530

    CGC: Icon update

--- capital_gain_calculator/build.ps1
+++ capital_gain_calculator/build.ps1
@@ -1,8 +1,8 @@
 flet build windows `
---project "CGS" `
+--project "CGC" `
 --company "Pooja ITR Center" `
 --description "A tool for Generating Capital Gain Data For ITR filing. Uses AIS's Capital Gain Data" `
---product "CGS" `
+--product "CGC" `
 --build-version "1.0.1" `
 --company "Pooja ITR Center" `
 --copyright "Copyright (C) 2025 Pooja ITR Center" `
