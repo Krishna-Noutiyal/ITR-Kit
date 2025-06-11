31d5c7602ab65240e1f04d55fd1cc24f0dff309e
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-06-11 15:06:22 +0530

    Dynamically Adding window's title in each software

--- form-16_generator/routes/router.py
+++ form-16_generator/routes/router.py
@@ -1,5 +1,7 @@
 import flet as ft
 from ui import MainView
+import toml
+import os
 
 class Router:
     def __init__(self, page: ft.Page):
@@ -7,7 +9,16 @@ class Router:
         self.setup_page()
     
     def setup_page(self):
-        self.page.title = "Sola V1.2"
+        # Get path to myproject.toml (one directory above)
+        toml_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "myproject.toml")
+        with open(toml_path, "r") as f:
+            data = toml.load(f)
+            
+        project = data.get("project", {})
+        name = project.get("name", "")
+        version = project.get("version", "")
+        
+        self.page.title = f"{name} v{version}"
         self.page.theme_mode = ft.ThemeMode.DARK
         self.page.auto_scroll = True
         self.page.window.height = 800
